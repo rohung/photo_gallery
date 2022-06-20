1a5d99aa6a48c79ead7c53958efd42c932c5be62
rohung <roderick.hung@gmail.com>
2022-06-20 00:11:19 -0700

    Second commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
+  
+ window.addEventListener('load', function() {
+    baguetteBox.run('.grid');
+});
 
- 
-baguetteBox.run('.gallery');
+document.getElementById("search").addEventListener("onkeyup", gallerySearch);
 
+function gallerySearch() {
+    
+  let input = document.getElementById("search").value.toLowerCase();
+  let pics = document.querySelectorAll(".image");
+    
+    for(i = 0; i < pics.length; i++) {
+      let pic = pics[i];
+      let caption =  pic.getAttribute("data-caption").toLowerCase();
+      if(caption.indexOf(input) > -1) {
+        pics[i].style.display="";
+      } else {
+        pics[i].style.display="none";
+      }
+    }
+}
